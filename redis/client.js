196198f3d9a3d1198798e035614d9f8597035801
Mehmet Seven <mehmetseven0@gmail.com>
2017-09-08 21:24:10 +0300

    fix

--- redis/client.js
+++ redis/client.js
@@ -1,14 +1,32 @@
 // Redis connection
 let config = require('../config/env.json')[process.env.NODE_ENV || 'development'];
 let redis = require('redis');
-let client = redis.createClient(config.redis.PORT, config.redis.URI);
 
-client.on('connect', () => {
-	console.log('redis server was connected!');
-});
+if (process.env.NODE_ENV == 'production' ){
 
-client.on('error', () => {
-	console.log('error while creating the socket connection');
-});
+	let rtg   = require('url').parse(config.redis.URI);
+	let client = redis.createClient(rtg.port, rtg.hostname);
 
-module.exports = client;
+	client.auth(rtg.auth.split(':')[1]);
+
+	client.on('connect', () => {
+		console.log('redis server was connected!');
+	});
+
+	client.on('error', () => {
+		console.log('error while creating the socket connection');
+	});
+
+}else{
+
+	let client = redis.createClient(config.redis.PORT, config.redis.URI);
+
+	client.on('connect', () => {
+		console.log('redis server was connected!');
+	});
+
+	client.on('error', () => {
+		console.log('error while creating the socket connection');
+	});
+}
+
