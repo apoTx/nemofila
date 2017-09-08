fe154d7c718080946c449ff0ac566715c6307df6
Mehmet Seven <mehmetseven0@gmail.com>
2017-09-08 21:30:45 +0300

    fix

--- redis/client.js
+++ redis/client.js
@@ -2,9 +2,9 @@
 let config = require('../config/env.json')[process.env.NODE_ENV || 'development'];
 let redis = require('redis');
 
-if (process.env.NODE_ENV == 'development' ){
+if (process.env.NODE_ENV == 'production' ){
 
-	let rtg   = require('url').parse('redis://h:pcc52bf7ac18d85f1f5edb2b41cdeded48b7082b5ddfe187a60f727a294b48f39@ec2-52-21-74-44.compute-1.amazonaws.com:36109');
+	let rtg   = require('url').parse(config.redis.URI);
 	let client = redis.createClient(rtg.port, rtg.hostname);
 
 	client.auth(rtg.auth.split(':')[1]);
