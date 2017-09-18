df43e8e0029fafb5dc0d069f727b0ac0d743ef7b
Mehmet Seven <mehmetseven0@gmail.com>
2017-09-18 19:44:56 +0300

    date format moment

--- routes/detail.js
+++ routes/detail.js
@@ -1,16 +1,16 @@
 let express = require('express');
 let router = express.Router();
+let moment = require('moment');
 
 // Models
 let Ads = require('../models/ads');
 
 router.get('/:slug/:id', (req, res, next) => {
 	Ads.findById(req.params.id, (err, data) => {
-		if (err){
+		if (err)
 			return next(err);
-		}else{
-			res.render( 'detail', { title: data.title, data: data });
-		}
+
+		res.render( 'detail', { title: data.title, data: data, moment: moment });
 	});
 });
 
