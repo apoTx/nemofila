5b3981c2efe1cd8b305a33a370e1553620cde75c
Mehmet Seven <mehmetseven0@gmail.com>
2017-10-09 14:46:25 +0300

    test

--- routes/detail.js
+++ routes/detail.js
@@ -47,12 +47,14 @@ router.get('/:slug/:id', (req, res, next) => {
 				'user.name': 1,
 				'user._id': 1,
 				'user.surname': 1,
-				'user.phone': 1,
+				'user.phone': 1
 			},
 		},
 	], (err, result)=>{
 		if (err)
 			return next(err);
+
+		console.log(result);
 
 		if(result.length < 1){
 			res.status(404).render('error/404', { message: 'Ad Not Found' });
@@ -63,12 +65,16 @@ router.get('/:slug/:id', (req, res, next) => {
 				title: data.title,
 				data: data,
 				moment: moment,
-				url: req.protocol + '://' + req.get('host') + req.originalUrl
+				url: req.protocol + '://' + req.get('host') + req.originalUrl,
+				session: req.session.user
 			};
 
+			console.log(object.session);
+
 			if( data.status === false ){
-				if ( String(data.ownerId) == req.session.user._id || req.session.user.isAdmin ){
-					res.render( 'detail', object);
+				if ( req.session.user ){
+					if (String(data.ownerId) == req.session.user._id || req.session.user.isAdmin)
+						res.render( 'detail', object);
 				}else{
 					res.status(404).render('error/404', { message: 'Ad Not Found' });
 				}
