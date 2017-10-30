9d4c48cada3934da04a8e66f738e9a4214acf76d
Mehmet Seven <mehmetseven0@gmail.com>
2017-10-30 20:20:08 +0300

    detail page

--- routes/profile/profile.js
+++ routes/profile/profile.js
@@ -1,11 +1,14 @@
 let express = require('express');
 let router = express.Router();
-
+let config = require('../../config/env.json')[process.env.NODE_ENV || 'development'];
 let requireLogin = require('../inc/requireLogin.js');
 
 /* GET users listing. */
 router.get('/', requireLogin, (req, res) => {
-	res.render( 'profile', { title: 'Profile' });
+	res.render( 'profile', {
+		title: 'Profile',
+		amazon_base_url: config.amazon_s3.photo_base_url
+	});
 });
 
 module.exports = router;
