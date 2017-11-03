0649c23e53716e1c2ac35f0f39a79552efcc1ba8
Mehmet Seven <mehmetseven0@gmail.com>
2017-11-04 02:52:45 +0300

    ads edit controller created

---
+++ public/js/admin/app/controllers/adsEditController.js
@@ -0,0 +1,40 @@
+app.controller('adsEditController', ['$scope', '$http', 'adsFactory', '$window', ($scope, $http, adsFactory, $window) => {
+
+	$scope.adEditForm = { };
+	$scope.adEditForm.publishStatus = '1';
+	$scope.adEditForm.reasonVisible = false;
+
+	$scope.changeStatus = () => {
+		if ($scope.adEditForm.publishStatus === '2')
+			$scope.adEditForm.reasonVisible = true;
+		else
+			$scope.adEditForm.reasonVisible = false;
+	};
+
+	$scope.submitEdit = () => {
+		$scope.loadingEditSubmit = true;
+		adsFactory.publishAd($scope.adEditForm).then((response) => {
+			console.log(response);
+			$scope.loadingEditSubmit = false;
+
+			if (response.status === 1){
+				$window.location.reload();
+			}
+		});
+	};
+
+	$scope.unpublish = () => {
+		let r = confirm('Are you sure?');
+		if (r === true) {
+			$scope.loadingUnpublish = true;
+			adsFactory.unpublish($scope.adEditForm.id).then((response) => {
+				$scope.loadingUnpublish = false;
+
+				if (response.status === 1){
+					$window.location.reload();
+				}
+			});
+		}
+	};
+
+}]);
