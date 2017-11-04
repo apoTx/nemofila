ec9cc8273dfd4dc3a2335ac851de89f282068368
Mehmet Seven <mehmetseven0@gmail.com>
2017-11-04 13:04:42 +0300

    admin ad detail poowers

--- routes/manage/ads.js
+++ routes/manage/ads.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let router = express.Router();
-
+let mongoose = require('mongoose');
+let moment = require('moment')
 // Models
 let Ads = require('../../models/ads');
 let Users = require('../../models/users');
@@ -19,10 +20,88 @@ router.get('/', requireLogin, (req, res) => {
 });
 
 router.get('/edit/:id', requireLogin, (req, res, next) => {
+	Ads.aggregate([
+		{
+			'$match': {
+				'_id': mongoose.Types.ObjectId(req.params.id),
+			}
+		},
+		// Power collection
+		{
+			$lookup: {
+				from: 'powers',
+				localField: '_id',
+				foreignField: 'adId',
+				as: 'power'
+			}
+		},
+		{
+			$unwind: {
+				path: '$power',
+				// ad collection, power collectionda herhangi eşleşme yapamasa bile ad'i döndür.
+				preserveNullAndEmptyArrays: true
+			}
+		},
+
+		{
+			$group: {
+				_id: {
+					_id: '$_id',
+					title: '$title',
+					status: '$status',
+					statusText: '$statusText',
+					slug: '$slug',
+					createdAt: '$createdAt',
+				},
+				power: {
+					$push: '$power'
+				},
+				totalActivePower: {
+					$sum: { $cond: [{ $gte: [ '$power.endingAt', new Date() ] }, '$power.powerNumber', 0] }
+				},
+				totalEndingPower: {
+					$sum: { $cond: [{ $lte: [ '$power.endingAt', new Date() ] }, '$power.powerNumber', 0] }
+				},
+				totalPower: {
+					$sum: '$power.powerNumber'
+				},
+			}
+		},
+
+		{
+			'$project': {
+				'_id': '$_id._id',
+				'title': '$_id.title',
+				'status': '$_id.status',
+				'statusText': '$_id.statusText',
+				'slug': '$_id.slug',
+				'createdAt': '$_id.createdAt',
+				power: 1,
+				totalActivePower: 1,
+				totalEndingPower: 1,
+				totalPower: 1
+			},
+		},
+	], (err, data) => {
+		if (err)
+			throw new Error(err);
+
+		let result = data[0];
+		console.log(result);
+		res.render('manage/ads/ad_edit', {
+			title: result.title,
+			data: result,
+			statusText: getAdStatusText(result.status),
+			moment: moment
+		});
+	});
+
+	/*
 	Ads.findById( req.params.id ,(err,result) => {
 		if (err)
 			next();
 
+		console.log(result);
 		if (result)
 			res.render('manage/ads/ad_edit', {
 				title: result.title,
@@ -31,7 +110,7 @@ router.get('/edit/:id', requireLogin, (req, res, next) => {
 			});
 		else
 			next();
-	});
+	});*/
 });
 
 router.post('/publishAd', requireLogin, (req, res) => {
@@ -160,7 +239,7 @@ router.get('/getAllAds', requireLogin, (req, res, next) => {
 			},
 		},
 		{ $sort : { _id : -1 } },
-	], (err, result)=>{
+	], (err, result) => {
 		if (err)
 			return next(err);
 
