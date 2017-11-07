d260eabac57769a8926d6189dd3bfed7201b3cc2
Mehmet Seven <mehmetseven0@gmail.com>
2017-11-07 14:55:44 +0300

    bug fix

--- routes/index.js
+++ routes/index.js
@@ -75,7 +75,7 @@ router.post('/login', (req,res) => {
 	});
 });
 
-router.post('/charge', requireLogin,  (req, res) => {
+router.post('/charge',  (req, res) => {
 	let amount = parseInt(req.body.amount) * 1000;
 
 	stripe.customers.create({
