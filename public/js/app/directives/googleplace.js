1bae2d1d386b3b68d27a8baa77668c51ea9bc122
Mehmet Seven <mehmetseven0@gmail.com>
2018-01-07 15:16:38 +0300

    lint

--- public/js/app/directives/googleplace.js
+++ public/js/app/directives/googleplace.js
@@ -1,15 +1,15 @@
-app.directive('googleplace', function() {
+app.directive('googleplace', () => {
 	return {
 		require: 'ngModel',
 		link: function(scope, element, attrs, model) {
-			var options = {
+			let options = {
 				types: [],
 				componentRestrictions: {}
 			};
 			scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
 
-			google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
-				scope.$apply(function() {
+			google.maps.event.addListener(scope.gPlace, 'place_changed', () => {
+				scope.$apply(() => {
 					model.$setViewValue(element.val());
 				});
 			});
