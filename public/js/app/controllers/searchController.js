73491a37c6ddef44c41c552de283a59495e66012
Mehmet Seven <mehmetseven0@gmail.com>
2018-02-05 00:17:10 +0300

    fix

--- public/js/app/controllers/searchController.js
+++ public/js/app/controllers/searchController.js
@@ -6,4 +6,12 @@ app.controller('searchController',  ['$scope', '$http', 'categoriesFactory', ($s
 			$scope.categories = response;
 		});
 	};
+
+	$scope.$watch('place', () => {
+		console.log($scope.place);
+		if ($scope.place.address_components){
+			$scope.placeLongName = $scope.place.address_components[0].long_name;
+		}
+	});
+
 }]);
