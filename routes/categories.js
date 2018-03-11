530f460271accf15f63a80d3295be1853d4b9ec1
Mehmet Seven <mehmetseven0@gmail.com>
2018-03-11 15:53:54 +0300

    translate

--- routes/categories.js
+++ routes/categories.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const Categories = require('../models/categories');
 
+
+// translate
 const TJO = require('translate-json-object')();
 
 TJO.init({
@@ -13,14 +15,13 @@ router.get('/getCategories', (req,res) => {
 	Categories.find({ $or: [ { type: { $exists : false } }, { type: false } ]   }, (err, data) => {
 		const locale = req.cookies['locale'];
 
-		if (!locale){
+		if (!locale || locale == 'en'){
 			res.json(data);
 		}else{
 			const obj = JSON.parse(JSON.stringify(data));
 
 			TJO.translate(obj, locale)
 				.then((data) => {
-					console.log(data);
 					res.json(data);
 				}).catch((err) => {
 					console.log('error ', err);
@@ -31,7 +32,20 @@ router.get('/getCategories', (req,res) => {
 
 router.get('/getEventCategories', (req,res) => {
 	Categories.find({ type: 1 }, (err, data) => {
-		res.json( data );
+		const locale = req.cookies['locale'];
+
+		if (!locale || locale == 'en') {
+			res.json(data);
+		} else {
+			const obj = JSON.parse(JSON.stringify(data));
+
+			TJO.translate(obj, locale)
+				.then((data) => {
+					res.json(data);
+				}).catch((err) => {
+				console.log('error ', err);
+			});
+		}
 	});
 });
 
