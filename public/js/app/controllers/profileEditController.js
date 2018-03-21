d1eb95c6f42c20bed9045ddf5d03559213d2c1cf
Mehmet Seven <mehmetseven0@gmail.com>
2018-03-21 22:14:15 +0300

    test

--- public/js/app/controllers/profileEditController.js
+++ public/js/app/controllers/profileEditController.js
@@ -14,26 +14,17 @@ app.controller('profileEditController', ['$scope', 'Upload', '$timeout', 'profil
 		profileEditFactory.get_s3_signature().then(data => {
 			const inputs = data.inputs;
 
-			uploadStart(dataUrl, name, inputs['X-amz-signature'], inputs['X-amz-credential'], inputs['X-amz-algorithm'], inputs['X-amz-date']);
+			uploadStart(dataUrl, name, inputs);
 		});
 	};
 
-	function uploadStart (dataUrl, name, x_amz_signature, x_amz_credential, x_amz_algorithm, x_amz_date){
-		console.log('a');
+	function uploadStart (dataUrl, name, inputs){
+		console.log(inputs['policy']);
 		Upload.upload({
-			url: config.s3_upload_url,
-			method: 'POST',
+			url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
 			data: {
-				key: 'test.jpg', // the key to store the file on S3, could be file name or customized
-				acl: $scope.acl, // sets the access to the uploaded file in the bucket: private, public-read, ...
-				policy: $scope.policy, // base64-encoded json policy (see article below)
-				'X-amz-signature': x_amz_signature, // base64-encoded signature based on policy string (see article below)
-				'X-amz-credential': x_amz_credential,
-				'X-amz-algorithm': x_amz_algorithm,
-				'X-amz-date': x_amz_date,
-				filename: 'test.jpg', // this is needed for Flash polyfill IE8-9
-				file: Upload.dataUrltoBlob(dataUrl, name),
-			}
+				file: Upload.dataUrltoBlob(dataUrl, name)
+			},
 		}).then((response) => {
 			$timeout(() => {
 				$scope.result = response.data;
