835dadac6dae0e58cf4932c74ffd7857251030fd
Mehmet Seven <mehmetseven0@gmail.com>
2018-03-21 19:46:58 +0300

    test

--- public/js/app/controllers/profileEditController.js
+++ public/js/app/controllers/profileEditController.js
@@ -10,22 +10,4 @@ app.controller('profileEditController', ['$scope', 'Upload', '$timeout', ($scope
 		$scope.openUploadProfilePictureModal();
 	};
 
-	$scope.upload = function (dataUrl, name) {
-		Upload.upload({
-			url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-			data: {
-				file: Upload.dataUrltoBlob(dataUrl, name)
-			},
-		}).then((response) => {
-			$timeout(() => {
-				$scope.result = response.data;
-			});
-		}, (response) => {
-			if (response.status > 0) $scope.errorMsg = response.status
-				+ ': ' + response.data;
-		}, (evt) => {
-			$scope.progress = parseInt(100.0 * evt.loaded / evt.total);
-		});
-	};
-
 }]);
