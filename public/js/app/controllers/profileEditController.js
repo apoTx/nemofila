0e3829beeda276946b8fa793c5def372b9d44f3e
Mehmet Seven <mehmetseven0@gmail.com>
2018-03-21 21:51:45 +0300

    tets

--- public/js/app/controllers/profileEditController.js
+++ public/js/app/controllers/profileEditController.js
@@ -1,5 +1,5 @@
 /*eslint-disable */
-app.controller('profileEditController', ['$scope', 'Upload', '$timeout', ($scope, Upload, $timeout) => {
+app.controller('profileEditController', ['$scope', 'Upload', '$timeout', 'profileEditFactory', 'config', ($scope, Upload, $timeout, profileEditFactory, config) => {
 /*eslint-enable */
 
 	$scope.openUploadProfilePictureModal = () => {
@@ -10,22 +10,40 @@ app.controller('profileEditController', ['$scope', 'Upload', '$timeout', ($scope
 		$scope.openUploadProfilePictureModal();
 	};
 
-	$scope.upload = function (dataUrl, name) {
+	$scope.upload = (dataUrl, name) => {
+		profileEditFactory.get_s3_signature().then(data => {
+			const inputs = data.inputs;
+
+			uploadStart(dataUrl, name, inputs['X-amz-signature'], inputs['X-amz-credential'], inputs['X-amz-algorithm'], inputs['X-amz-date']);
+		});
+	};
+
+	function uploadStart (dataUrl, name, x_amz_signature, x_amz_credential, x_amz_algorithm, x_amz_date){
+		console.log('a');
 		Upload.upload({
-			url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+			url: config.s3_upload_url,
+			method: 'POST',
 			data: {
-				file: Upload.dataUrltoBlob(dataUrl, name)
-			},
-		}).then(function (response) {
-			$timeout(function () {
+				key: 'test.jpg', // the key to store the file on S3, could be file name or customized
+				acl: $scope.acl, // sets the access to the uploaded file in the bucket: private, public-read, ...
+				policy: $scope.policy, // base64-encoded json policy (see article below)
+				'X-amz-signature': x_amz_signature, // base64-encoded signature based on policy string (see article below)
+				'X-amz-credential': x_amz_credential,
+				'X-amz-algorithm': x_amz_algorithm,
+				'X-amz-date': x_amz_date,
+				filename: 'test.jpg', // this is needed for Flash polyfill IE8-9
+				file: Upload.dataUrltoBlob(dataUrl, name),
+			}
+		}).then((response) => {
+			$timeout(() => {
 				$scope.result = response.data;
 			});
-		}, function (response) {
+		}, (response) => {
 			if (response.status > 0) $scope.errorMsg = response.status
 				+ ': ' + response.data;
-		}, function (evt) {
+		}, (evt) => {
 			$scope.progress = parseInt(100.0 * evt.loaded / evt.total);
 		});
-	}
+	};
 
 }]);
--- public/js/app/main.js
+++ public/js/app/main.js
@@ -15,5 +15,6 @@ let app = angular.module('app',[
 
 app.value('config', {
 	's3_upload_url': 'https://easyad-static.s3-eu-central-1.amazonaws.com',
+	's3_upload_signature_service_url': 'http://jqueryegitimseti.com/amazon-service.php',
 });
 /*eslint-enable */
