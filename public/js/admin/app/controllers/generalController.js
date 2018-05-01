ed33985431f377b6dd7ba4d78ff8b4262fa70782
Mehmet Seven <mehmetseven0@gmail.com>
2018-05-01 12:45:06 +0300

    test

--- public/js/admin/app/controllers/generalController.js
+++ public/js/admin/app/controllers/generalController.js
@@ -3,7 +3,6 @@ app.controller('generalController', ['$scope', 'Upload', 'config', '$timeout', '
 	$scope.getUploadCredentials = (file, errFiles) => {
 		generalFactory.get_s3_signature().then(data => {
 			$scope.uploadFiles(file, errFiles, data.inputs);
-			console.log(data.inputs);
 		});
 	};
 
@@ -13,8 +12,20 @@ app.controller('generalController', ['$scope', 'Upload', 'config', '$timeout', '
 		$scope.errFile = errFiles && errFiles[0];
 		if (file) {
 			file.upload = Upload.upload({
-				url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-				data: { file: file }
+				url: config.s3_upload_url,
+				method: 'POST',
+				data: {
+					key: 'header.jpg', // the key to store the file on S3, could be file name or customized
+					acl: inputs.acl, // sets the access to the uploaded file in the bucket: private, public-read, ...
+					policy: inputs.policy, // base64-encoded json policy (see article below)
+					'X-amz-signature': inputs['X_amz_signature'], // base64-encoded signature based on policy string (see article below)
+					'X-amz-credential': inputs['x_amz_credential'],
+					'X-amz-algorithm': inputs['X_amz_algorithm'],
+					'X-amz-date': inputs['X_amz_date'],
+					'Content-Type': file.type != '' ? file.type : 'application/octet-stream', // content type of the file (NotEmpty)
+					filename: 'header.jpg', // this is needed for Flash polyfill IE8-9
+					file: file,
+				}
 			});
 
 			file.upload.then((response) => {
